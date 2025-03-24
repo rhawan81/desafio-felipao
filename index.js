18f4e6ed77a0b5efefb59a7db7aadfbe6fc20ce8
rhawan81 <rhawanjs@gmail.com>
2025-03-24 18:54:46 -0300

    Meu segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("bem vindo")
+let heroi = " Reyna "
+let QntdDeExp = 9000
+
+if(QntdDeExp < 1000){
+
+   console.log (heroi + "esta no Rank de Ferro " )
+} 
+
+   else if (QntdDeExp <= 2001){
+  
+  console.log(heroi + "esta no Rank  de Prata ") 
+  
+  }  else if (QntdDeExp <= 5001) 
+  
+      console.log(heroi + "esta no Rank  de Ouro " )
+      
+     else if(QntdDeExp >= 9000) {
+     
+     
+     console.log(heroi + "esta no Rank  de Diamante ")
+      } 
+      
+      else {
+       console.log( heroi + "esta no Rank  de Imortal") 
+       } 
+      
